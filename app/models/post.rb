5e51a98f0550b6587f0aff68ff4cbb066278c062
omokehinde <engr.omokehinde@gmail.com>
2022-05-03 23:58:19 +0100

    User with devise

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
     validates :title, presence: true, length: {minimum: 5, maximum: 50}
     validates :body, presence: true, length: {minimum: 30, maximum: 2500}
+
+    belongs_to :user
 end
