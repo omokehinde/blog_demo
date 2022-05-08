43e13392cc2f74f6ced3f46f9df22afa47a00acc
omokehinde <engr.omokehinde@gmail.com>
2022-05-08 23:15:54 +0100

    Add post comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ApplicationRecord
     validates :body, presence: true, length: {minimum: 30, maximum: 2500}
 
     belongs_to :user
+    has_many :comments, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,12 @@ Rails.application.routes.draw do
   }
   get '/u/:id', to: 'users#profile', as: 'user'
 
-  resources :posts
+  # /post/:id/comments/:id
+  resources :posts do
+    resources :comments
+  end
+
+
   get 'about', to: 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
